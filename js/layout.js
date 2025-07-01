bc3cebb2a995a427e1e09db48f33a6c3b4b330f2
DaviMeurer <meurerdavi569@gmail.com>
2025-06-30 21:52:59 -0400

    edit "historiadeponta"

--- js/layout.js
+++ js/layout.js
@@ -1,18 +1,18 @@
 
 document.addEventListener("DOMContentLoaded", ()=> {
 
-    fetch("./header.html")
+    fetch("../header.html")
         .then((response)=>response.text())
         .then((data)=>{
             document.getElementById("header").innerHTML=data
         })
         .catch((error) => console.error("Erro ao carregar o header", error))
 
-    fetch("./footer-v2.html")
+    fetch("../footer-v2.html")
         .then((response)=>response.text())
         .then((data)=>{
             document.getElementById("footer").innerHTML=data
         })
-        .catch((error) => console.error("Erro ao carregar o header", error))
+        .catch((error) => console.error("Erro ao carregar o footer", error))
 
 })
